f2b4a141df46304ab9df0ade30be572d0ac727b0
Ben Cross <bencross323@gmail.com>
2018-06-06 10:34:45 -0500

    working app with styling and debounced search function

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,7 +10,8 @@ class SearchBar extends Component {
     render() {
         return (
             <div className="search-bar">
-                <input 
+                <input
+                placeholder="Search for Videos"
                 value={this.state.term} 
                 onChange={event => this.onInputChange(event.target.value)} />
             </div>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-// Create new component. This component should produce
-// some HTML
-const App = () => {
-    return <div>Hi!</div>;
-};
-// Take this component generated HTML and put it in the DOM(on the page)
+const API_KEY = 'AIzaSyDpOyVzBTH5jHkmw1ni9BM-t0A7cxuMl-k';
+
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+         };
+
+         this.videoSearch('Mountain Biking');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+    render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 500);
+
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
+}
+
 ReactDOM.render(<App />, document.querySelector('.container '));
